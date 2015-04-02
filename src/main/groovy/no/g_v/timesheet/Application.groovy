e4cbc4ae11cfd8b0f03f37da51a82f1b1d90ee7e
Andreas Pedro Garcia-Vedal <ap@g-v.no>
2015-04-02 16:33:33 +0200

    Initial commit

---
+++ src/main/groovy/no/g_v/timesheet/Application.groovy
@@ -0,0 +1,13 @@
+package no.g_v.timesheet
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class
+        )
+    }
+}
