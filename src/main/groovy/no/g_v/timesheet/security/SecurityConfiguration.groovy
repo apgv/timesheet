9c5e98d9ec6b27b7aadea9702af40ede75d6a640
Andreas Pedro Garcia-Vedal <ap@g-v.no>
2015-04-03 16:23:17 +0200

    replaced in-memory authentication with jdbc authentication, replaced in-memory database with MySQL and added flyway migration

--- src/main/groovy/no/g_v/timesheet/security/SecurityConfiguration.groovy
+++ src/main/groovy/no/g_v/timesheet/security/SecurityConfiguration.groovy
@@ -5,12 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+import javax.sql.DataSource
 
 @Configuration
 class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    void configureGlobal(AuthenticationManagerBuilder auth) {
-        auth.inMemoryAuthentication().withUser("user").password("pass").roles("USER")
+    void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource) {
+        auth.jdbcAuthentication()
+            .passwordEncoder(new BCryptPasswordEncoder())
+            .dataSource(dataSource)
     }
 }
