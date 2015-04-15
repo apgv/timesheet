31db56bf20cbdbf5352a82fb61d1a6ba51eb827e
Andreas Pedro Garcia-Vedal <ap@g-v.no>
2015-04-15 22:04:47 +0200

    Formatted the file

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,12 +1,12 @@
-create table users(
-    username varchar(50) not null primary key,
-    password varchar(60) not null,
-    enabled boolean not null
+CREATE TABLE users (
+  username VARCHAR(50) NOT NULL PRIMARY KEY,
+  password VARCHAR(60) NOT NULL,
+  enabled  BOOLEAN     NOT NULL
 );
 
-create table authorities (
-    username varchar(50) not null,
-    authority varchar(50) not null,
-    constraint fk_authorities_users foreign key(username) references users(username)
+CREATE TABLE authorities (
+  username  VARCHAR(50) NOT NULL,
+  authority VARCHAR(50) NOT NULL,
+  CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users (username)
 );
-create unique index ix_auth_username on authorities (username,authority);
+CREATE UNIQUE INDEX ix_auth_username ON authorities (username, authority);
