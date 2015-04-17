55b8c2f971e71a983e46535f5413e10e742aec81
Andreas Pedro Garcia-Vedal <ap@g-v.no>
2015-04-17 12:20:04 +0200

    Changed the inline password encoder to a bean

--- src/main/groovy/no/g_v/timesheet/security/SecurityConfiguration.groovy
+++ src/main/groovy/no/g_v/timesheet/security/SecurityConfiguration.groovy
@@ -1,7 +1,7 @@
 package no.g_v.timesheet.security
 
-
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -13,9 +13,16 @@ import javax.sql.DataSource
 class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource) {
+    void configureGlobal(AuthenticationManagerBuilder auth,
+                         BCryptPasswordEncoder bCryptPasswordEncoder,
+                         DataSource dataSource) {
         auth.jdbcAuthentication()
-            .passwordEncoder(new BCryptPasswordEncoder())
+            .passwordEncoder(bCryptPasswordEncoder)
             .dataSource(dataSource)
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        new BCryptPasswordEncoder()
     }
 }
