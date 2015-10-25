f3fa061cd1299651292e4103f25861ad20ae186d
Andreas Pedro Garcia-Vedal <ap@g-v.no>
2015-10-25 18:33:53 +0100

    Added project entity.

---
+++ src/main/groovy/no/g_v/timesheet/domain/Project.groovy
@@ -0,0 +1,20 @@
+package no.g_v.timesheet.domain
+
+import javax.persistence.*
+
+@Entity
+@Table(name = 'projects')
+class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = 'project_id')
+    Long id
+
+    @Column(name = 'name', nullable = false)
+    String name
+
+    @OneToOne
+    @JoinColumn(name = 'person_id')
+    Person owner
+}
