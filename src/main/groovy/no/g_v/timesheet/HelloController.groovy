f5df38aa63615ea7dbc56b6d65ba0ac200b99098
Andreas Pedro Garcia-Vedal <ap@g-v.no>
2015-11-22 00:19:59 +0100

    Changed HelloController to use the new GreetingService.

--- src/main/groovy/no/g_v/timesheet/HelloController.groovy
+++ src/main/groovy/no/g_v/timesheet/HelloController.groovy
@@ -1,5 +1,7 @@
 package no.g_v.timesheet
 
+import no.g_v.timesheet.service.GreeterService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloController {
 
+    @Autowired
+    private GreeterService greeterService
+
     @RequestMapping(value = '/greeting', method = RequestMethod.GET)
     Map<String, String> hello() {
-        [greeting: 'Hello from Spring']
+        [greeting: greeterService.greet('Spring controller')]
     }
 }
